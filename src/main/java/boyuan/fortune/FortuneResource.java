ea83f89822c467af38774cee8aba12e5c2c0fca4
555isfaiz <boyuan.xiao_aka_mark@outlook.com>
2026-07-05 10:01:07 +0200

    init

---
+++ src/main/java/boyuan/fortune/FortuneResource.java
@@ -0,0 +1,8 @@
+package boyuan.fortune;
+
+import jakarta.ws.rs.Path;
+
+public class FortuneResource {
+
+    
+}
---
+++ src/test/java/boyuan/fortune/FortuneResourceTest.java
@@ -0,0 +1,8 @@
+package boyuan.fortune;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class FortuneResourceTest {
+
+}
